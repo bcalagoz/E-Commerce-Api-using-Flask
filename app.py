84726e41b7bc6b8f838d51d6e14e195fa93bc854
bcalagoz <bcalagoz@icloud.com>
2023-03-08 17:52:19 +0300

    create /product

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from routes.store import store_bp
+from routes.product import product_bp
 from routes.auth import auth_bp
+
+
 app = Flask(__name__)
 
 app.register_blueprint(store_bp, url_prefix='/store')
+app.register_blueprint(product_bp, url_prefix='/product')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
