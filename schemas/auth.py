5aee8be3c81b30eb2c7cba6a617580b16d3ff82a
bcalagoz <bcalagoz@icloud.com>
2023-04-03 13:23:47 +0300

    add sign_up_schema for validation with REGEX

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,5 +1,3 @@
-# TODO regex ile email, userid ... kontrol et
-
 login_schema = {
     'type': 'object',
     'properties': {
@@ -7,4 +5,37 @@ login_schema = {
         'password': {'type': 'string'},
     },
     'required': ['email', 'password']
+}
+
+sign_up_schema = {
+    "title": "User Signup Form Schema",
+    "description": "A JSON schema for validating user signup form data.",
+    "type": "object",
+    "properties": {
+        "first_name": {
+            "type": "string",
+            "description": "The user's first name.",
+            "pattern": "^[a-zA-ZçÇğĞıİöÖşŞüÜ]+$",
+            "maxLength": 50
+        },
+        "last_name": {
+            "type": "string",
+            "description": "The user's last name.",
+            "pattern": "^[a-zA-ZçÇğĞıİöÖşŞüÜ]+$",
+            "maxLength": 50
+        },
+        "email": {
+            "type": "string",
+            "description": "The user's email address.",
+            "maxLength": 255,
+            "pattern": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+        },
+        "password": {
+            "type": "string",
+            "description": "The user's password.",
+            "pattern": "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            "maxLength": 100
+        }
+    },
+    "required": ["first_name", "last_name", "email", "password"]
 }
