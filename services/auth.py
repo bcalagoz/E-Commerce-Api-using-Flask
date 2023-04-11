95fd2a7ca0aebeb1802fac61849dc834e0aca490
bcalagoz <bcalagoz@icloud.com>
2023-04-11 19:19:42 +0300

    create AuthService and make sign_up with it

---
+++ services/auth.py
@@ -0,0 +1,59 @@
+from db.auth import Auth, User
+from db import get_connection_conn_cursor, execute_without_commit, commit_without_execute
+from utils.functions import hash_password
+import uuid
+from flask import jsonify
+from utils.functions import create_token
+# from controllers.auth import send_verification_email
+
+
+class AuthService:
+    auth_db = None
+    user_db = None
+
+    def __init__(self):
+        self.auth_db = Auth()
+        self.user_db = User()
+
+    def sign_up(self, new_account_data):
+        try:
+            new_account_data['password'] = hash_password(new_account_data['password'])
+            new_account_data['id'] = uuid.uuid4().hex
+
+            if self.user_db.get_user_by_email(new_account_data['email']):
+                return jsonify({'error': 'Email already exists!'}), 400
+
+            conn, cur = get_connection_conn_cursor()
+            query = 'INSERT INTO users (id, first_name, last_name, email, password, role) VALUES (%s, %s, %s, %s, %s, %s)'
+            args = (new_account_data['id'], new_account_data['first_name'], new_account_data['last_name'], new_account_data['email'], new_account_data['password'], "customer")
+
+            if not execute_without_commit(conn, cur, query, args):
+                raise Exception('Error executing SQL query')
+
+            # Create session_key
+            session_key = uuid.uuid4().hex
+
+            access_token = create_token("access", new_account_data['id'], session_key)
+            refresh_token = create_token("refresh", new_account_data['id'], session_key)
+            verify_token = create_token("verify", new_account_data['id'], session_key, new_account_data['email'])
+
+            args = ((uuid.uuid4().hex, new_account_data['id'], session_key, "refresh"), (uuid.uuid4().hex, new_account_data['id'], session_key, "verify"))
+            for arg in args:
+                query = 'INSERT INTO auth (id, user_id, session_key, token_type) VALUES (%s, %s, %s, %s)'
+                if not execute_without_commit(conn, cur, query, arg):
+                    raise Exception('Error executing SQL query')
+
+            if not commit_without_execute(conn, cur):
+                raise Exception('Error committing transaction')
+
+            # TODO send verification mail
+            # if not send_verification_email(verify_token, new_user.email):
+            #     return jsonify({'error': 'Email gönderilemedi'}), 500
+            # send_verification_email(verify_token, new_account_data['email'])
+
+            return {'access_token': access_token, 'refresh_token': refresh_token, 'verify_token': verify_token,
+                    'user_id': new_account_data['id']}, 201
+
+        except Exception as e:
+            print(e)
+            return jsonify({'error': 'An error occurred while processing your request.'}), 500
