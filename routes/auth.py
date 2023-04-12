cc08a820ba52c3b9d249e35d7f05b39205d8b81e
bcalagoz <bcalagoz@icloud.com>
2023-04-12 17:52:33 +0300

    create new endpoints

--- routes/auth.py
+++ routes/auth.py
@@ -1,12 +1,17 @@
 from flask import Blueprint
-from controllers.auth import sign_up, login, logout
+from controllers.auth import sign_up, login, logout, refresh, verify, verify_email
 
 
 auth_bp = Blueprint('user', __name__)
 
-auth_bp.route('/sign-up', methods=['GET', 'POST'])(sign_up)
-auth_bp.route('/login', methods=['GET', "POST"])(login)
-auth_bp.route('/logout', methods=['GET', "POST"])(logout)
+auth_bp.route('/sign-up', methods=["GET", "POST"])(sign_up)
+auth_bp.route('/login', methods=["GET", "POST"])(login)
+auth_bp.route('/logout', methods=["GET", "POST"])(logout)
+auth_bp.route('/verify/<token>', methods=["GET"])(verify_email)
+auth_bp.route('/refresh', methods=["GET"])(refresh)
+auth_bp.route('/verify', methods=["GET"])(verify)
+
+
 
 
 
