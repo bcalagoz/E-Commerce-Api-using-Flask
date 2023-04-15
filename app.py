725975b3a72bb2521b6a4f578b468ade257556c4
bcalagoz <bcalagoz@icloud.com>
2023-04-15 16:42:27 +0300

    add create_app

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-from flask import Flask
-from routes.store import store_bp
-from routes.product import product_bp
-from routes.auth import auth_bp
-from routes.order import order_bp
+from create_app import create_app
 
-app = Flask(__name__)
-
-app.register_blueprint(store_bp, url_prefix='/store')
-app.register_blueprint(product_bp, url_prefix='/product')
-app.register_blueprint(order_bp, url_prefix='/order')
-app.register_blueprint(auth_bp, url_prefix='/auth')
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
