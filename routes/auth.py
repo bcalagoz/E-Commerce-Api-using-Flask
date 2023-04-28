627af577f8d4188a98ac2afbb3d7eb162763bb6c
bcalagoz <bcalagoz@icloud.com>
2023-04-28 14:01:46 +0300

    add /session endpoint

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.auth import sign_up, login, logout, refresh, verify, verify_email
+from controllers.auth import sign_up, login, logout, refresh, verify, verify_email, get_all_sessions
 
 
 auth_bp = Blueprint('auth', __name__)
@@ -10,6 +10,9 @@ auth_bp.route('/logout', methods=["GET", "POST"])(logout)
 auth_bp.route('/verify/<token>', methods=["GET"])(verify_email)
 auth_bp.route('/refresh', methods=["GET"])(refresh)
 auth_bp.route('/verify', methods=["GET"])(verify)
+auth_bp.route('/session', methods=["GET"])(get_all_sessions)
+
+
 
 
 
