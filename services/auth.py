41ff297a4fa524b65be34af5a7afd942df140de8
bcalagoz <bcalagoz@icloud.com>
2023-05-09 21:04:31 +0300

    create requirements.txt

--- services/auth.py
+++ services/auth.py
@@ -2,8 +2,10 @@ from db.auth import Auth, User
 from db import get_connection_conn_cursor, execute_without_commit, commit_without_execute
 from utils.functions import hash_password
 import uuid
-from flask import jsonify
+from flask import jsonify, json, request
 from utils.functions import create_token, send_verification_email
+from ua_parser import user_agent_parser
+import requests
 
 
 class AuthService:
@@ -30,6 +32,18 @@ class AuthService:
             if not execute_without_commit(conn, cur, query, args):
                 raise Exception('Error executing SQL query')
 
+            ua_string = request.user_agent.string
+            parsed_string = user_agent_parser.Parse(ua_string)
+            browser = parsed_string['user_agent']['family']  # browser
+            device = parsed_string['device']['model']  # device
+            os = parsed_string['os']['family']  # os
+            ip_adress = request.remote_addr  # ip_address
+            access_key = "4824fb5309db20b5a30db540c7066670"
+            url = f'http://api.ipstack.com/{ip_adress}?access_key={access_key}&format=1'
+            r = requests.get(url)
+            j = json.loads(r.text)
+            location = j['city']  # location
+
             # Create session_key
             session_key = uuid.uuid4().hex
 
@@ -37,9 +51,10 @@ class AuthService:
             refresh_token = create_token("refresh", new_account_data['id'], session_key, new_account_data['role'])
             verify_token = create_token("verify", new_account_data['id'], session_key, new_account_data['role'], new_account_data['email'])
 
-            args = ((uuid.uuid4().hex, new_account_data['id'], session_key, "refresh"), (uuid.uuid4().hex, new_account_data['id'], session_key, "verify"))
+            args = ((uuid.uuid4().hex, new_account_data['id'], session_key, "refresh", browser, device, os, ip_adress, location),
+                    (uuid.uuid4().hex, new_account_data['id'], session_key, "verify", browser, device, os, ip_adress, location))
             for arg in args:
-                query = 'INSERT INTO auth (id, user_id, session_key, token_type) VALUES (%s, %s, %s, %s)'
+                query = 'INSERT INTO auth (id, user_id, session_key, token_type, browser, device, os, ip_address, location) VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s)'
                 if not execute_without_commit(conn, cur, query, arg):
                     raise Exception('Error executing SQL query')
 
